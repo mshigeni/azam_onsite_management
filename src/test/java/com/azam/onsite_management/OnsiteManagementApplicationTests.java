0e45cc929f21425e02b0f8cef4f3462880c14b1e
Eng. Mshigeni L Mganga <mshigeni@gmail.com>
2025-09-25 18:46:43 +0300

    Azam Spring Boot API

---
+++ src/test/java/com/azam/onsite_management/OnsiteManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azam.onsite_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsiteManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
