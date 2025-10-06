ccff7831e61383027e9ee692894fd717d620de95
Eng. Mshigeni L Mganga <mshigeni@gmail.com>
2025-10-06 23:55:13 +0300

    Load dashboard summary, connect to RealTime data

--- src/main/kotlin/com/azam/onsite_management/models/Transaction.kt
+++ src/main/kotlin/com/azam/onsite_management/models/Transaction.kt
@@ -12,6 +12,30 @@ data class Transaction(
     val id: Long = 0,
 
     @Column(nullable = false)
+    val card_uid: String,
+
+    @Column(nullable = false)
+    val local_reference: String,
+
+    @Column(nullable = false)
+    val trx_reference: String,
+
+    @Column(nullable = false)
+    val amount: String,
+
+    @Column(nullable = false)
+    val comment: String,
+
+    @Column(nullable = false)
+    val mac_addr: String,
+
+    @Column(nullable = false)
+    val scanned_at: String,
+
+    @Column(nullable = false)
+    val paid_at: String,
+
+    @Column(nullable = false)
     val site: String,   // e.g., "MAGOGONI", "KIGAMBONI"
 
     @Column(nullable = false)
@@ -21,11 +45,11 @@ data class Transaction(
     val createdAt: LocalDateTime = LocalDateTime.now()
 
     @Column(name = "created_by", nullable = false)
-    val createdBy: LocalDateTime = LocalDateTime.now()
+    val createdBy: String
 
     @Column(name = "updated_at", nullable = false)
     val updatedAt: LocalDateTime = LocalDateTime.now()
 
     @Column(name = "updated_by", nullable = false)
-    val updatedBy: LocalDateTime = LocalDateTime.now()
+    val updatedBy: String
 )
