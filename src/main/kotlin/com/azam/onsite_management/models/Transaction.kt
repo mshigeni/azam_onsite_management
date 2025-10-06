5d2d1aa33b63886a7f99e7e5edf6aba4fbd0a797
Eng. Mshigeni L Mganga <mshigeni@gmail.com>
2025-10-06 23:54:53 +0300

    Load dashboard summary, connect to RealTime data

--- src/main/kotlin/com/azam/onsite_management/models/Transaction.kt
+++ src/main/kotlin/com/azam/onsite_management/models/Transaction.kt
@@ -12,11 +12,20 @@ data class Transaction(
     val id: Long = 0,
 
     @Column(nullable = false)
-    val gate: String,   // e.g., "MAGOGONI", "KIGAMBONI"
+    val site: String,   // e.g., "MAGOGONI", "KIGAMBONI"
 
     @Column(nullable = false)
     val status: String, // e.g., "ONLINE", "OFFLINE"
 
     @Column(name = "created_at", nullable = false)
     val createdAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "created_by", nullable = false)
+    val createdBy: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "updated_at", nullable = false)
+    val updatedAt: LocalDateTime = LocalDateTime.now()
+
+    @Column(name = "updated_by", nullable = false)
+    val updatedBy: LocalDateTime = LocalDateTime.now()
 )
